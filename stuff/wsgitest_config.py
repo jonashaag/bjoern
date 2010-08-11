f1a72cdeab87da7f9061a6e9c664bde43ffc834b
Jonas Haag <jonas@lophus.org>
2010-08-11 02:00:50 +0200

    Fix wsgitest config

--- stuff/wsgitest_config.py
+++ stuff/wsgitest_config.py
@@ -4,5 +4,8 @@ SERVER_PORT = 9998
 SERVER_BOOT_DURATION = 0.1
 
 def run_server(app, host, port):
-    bjoern.route('.*')(app)
-    bjoern.run(host, port)
+    if bjoern.HAVE_ROUTING:
+        bjoern.route('.*')(app)
+        bjoern.run(host, port)
+    else:
+        bjoern.run(app, host, port)
