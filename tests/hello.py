551943c54e78fb2fed1c873b59925380192e4875
Jonas Haag <jonas@lophus.org>
2010-10-24 21:23:06 +0200

    test/hello.py app now proxies a few other example apps

--- tests/hello.py
+++ tests/hello.py
@@ -1,15 +1,28 @@
 import bjoern
+from random import choice
 
-def wsgi_app(env, start_response):
-    start_response('200 abc', [('Content-Length', '12')])
-    #start_response('200 abc', [('Content-Length', '12')], ())
+def app1(env, sr):
+    sr('200 ok', [('Foo', 'Bar'), ('Blah', 'Blubb'), ('Spam', 'Eggs'), ('Blurg', 'asdasjdaskdasdjj asdk jaks / /a jaksdjkas jkasd jkasdj '),
+                  ('asd2easdasdjaksdjdkskjkasdjka', 'oasdjkadk kasdk k k k k k ')])
+    return ['hello', 'world']
+
+def app2(env, sr):
+    sr('200 ok', [])
+    return 'hello'
+
+def app3(env, sr):
+    sr('200 abc', [('Content-Length', '12')])
     yield 'Hello'
     yield ' World'
     yield '\n'
 
-if 0:
-    def wsgi_app(e, s):
-        s('200 ok', [])
-        return ['hello\n']
+def app4(e, s):
+    s('200 ok', [])
+    return ['hello\n']
+
+apps = (app1, app2, app3, app4)
+
+def wsgi_app(env, start_response):
+    return choice(apps)(env, start_response)
 
 bjoern.run(wsgi_app, '0.0.0.0', 8080)
