e687ad605f46fe0247dee73e29f334db72c1184f
Jonas Haag <jonas@lophus.org>
2012-02-27 19:38:45 +0100

    Fix imports

--- bjoern/portable_sendfile.c
+++ bjoern/portable_sendfile.c
@@ -1,12 +1,13 @@
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/sendfile.h>
 
 #define SENDFILE_CHUNK_SIZE 16*1024
 
 #ifdef __APPLE__
 
-  /* OS X sendfile */
+  /* OS X */
+
+  #include <sys/types.h>
+
   ssize_t portable_sendfile(int out_fd, int in_fd) {
     off_t len = SENDFILE_CHUNK_SIZE;
     if(sendfile(in_fd, out_fd, 0, &len, NULL, 0) == -1)
@@ -16,7 +17,10 @@
 
 #else
 
-  /* Linux sendfile */
+  /* Linux */
+
+  #include <sys/sendfile.h>
+
   ssize_t portable_sendfile(int out_fd, int in_fd) {
     return sendfile(out_fd, in_fd, NULL, SENDFILE_CHUNK_SIZE);
   }
