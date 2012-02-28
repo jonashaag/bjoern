1cca56e28589eb96be6f25266d3783a9e3067041
Patrick Lucas <me@patricklucas.com>
2012-02-27 19:18:47 -0500

    Quiet clang warnings
     - -Wno-array-bounds because it complains about 'PyTuple_GET_ITEM'-style macros
     - -Wno-unused-parameter since many functions don't have all their arguments
       used

--- setup.py
+++ setup.py
@@ -13,8 +13,9 @@ bjoern_extension = Extension(
     define_macros = [('WANT_SENDFILE', '1'),
                      ('WANT_SIGINT_HANDLING', '1')],
     extra_compile_args = ['-std=c99', '-fno-strict-aliasing', '-Wall',
-                          '-Wextra', '-Wno-unused', '-g', '-fPIC',
-                          '-Wno-missing-field-initializers', '-fcommon']
+                          '-Wextra', '-Wno-unused', '-Wno-unused-parameter',
+                          '-g', '-fPIC', '-Wno-missing-field-initializers',
+                          '-Wno-array-bounds', '-fcommon']
 )
 
 setup(
