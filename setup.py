ab8a3a7cce900221766d236f21cf421d6fe4372a
Won <longway68@gmail.com>
2013-08-16 22:03:38 +0800

    fix compiling error "bjoern/request.h:4:16: error：ev.h：no such file or directory" when pip install in Fedora/CentOS

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ bjoern_extension = Extension(
     'bjoern',
     sources       = SOURCE_FILES,
     libraries     = ['ev'],
-    include_dirs  = ['http-parser'],
+    include_dirs  = ['http-parser', '/usr/include/libev'],
     define_macros = [('WANT_SENDFILE', '1'),
                      ('WANT_SIGINT_HANDLING', '1')],
     extra_compile_args = ['-std=c99', '-fno-strict-aliasing', '-fcommon',
