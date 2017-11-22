52ac676ab458eb7c4c4620b8d25f1f410e10b202
Jonas Haag <jonas@lophus.org>
2017-11-22 20:46:23 +0100

    Test

--- tests/headers.py
+++ tests/headers.py
@@ -1,9 +1,9 @@
+# encoding: utf-8
 import pprint
 import bjoern
 
 def app(env, start_response):
-    pprint.pprint(env)
-    start_response('200 yo', [])
-    return []
+    start_response('200 yo', [('Content-Type', 'text/plain'), ('Ü', 'ä'), ('a'*1000, 'b'*1000)])
+    return [b'foo', b'bar']
 
 bjoern.run(app, '0.0.0.0', 8080)
