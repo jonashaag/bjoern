7bfb7eec850f88e1a67ac86cf985c70d2fcf6e8a
Jonas Haag <jonas@lophus.org>
2018-03-02 11:17:53 +0100

    Version 2.1.0

--- bjoern/_bjoernmodule.c
+++ bjoern/_bjoernmodule.c
@@ -79,11 +79,11 @@ PyMODINIT_FUNC INIT_BJOERN(void)
     return NULL;
   }
 
-  PyModule_AddObject(bjoern_module, "version", Py_BuildValue("(iii)", 2, 0, 5));
+  PyModule_AddObject(bjoern_module, "version", Py_BuildValue("(iii)", 2, 1, 0));
   return bjoern_module;
 #else
   PyObject* bjoern_module = Py_InitModule("_bjoern", Bjoern_FunctionTable);
-  PyModule_AddObject(bjoern_module, "version", Py_BuildValue("(iii)", 2, 0, 5));
+  PyModule_AddObject(bjoern_module, "version", Py_BuildValue("(iii)", 2, 1, 0));
 #endif
 
 }
