8b5c1e23fe1b84cd8b920262abfe100234fc4eeb
Dani G <dani@robbie.ai>
2019-06-15 13:46:18 +0200

    release 4.0.0rc1 improve changelog

--- bjoern/__init__.py
+++ bjoern/__init__.py
@@ -2,7 +2,7 @@ import logging
 import os
 import subprocess
 
-__version__ = "4.0.11"
+__version__ = "4.0.0rc1"
 MAX_LISTEN_BACKLOG = int(
     subprocess.run(["cat", "/proc/sys/net/core/somaxconn"], stdout=subprocess.PIPE)
     .stdout.decode()
