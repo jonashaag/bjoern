ee9edd18b6ff918dbe4311000619a8adba74bb54
Gufran <mohammad.gufran@shuttl.com>
2019-10-04 16:09:50 +0530

    Get rid of cleanup() function

--- bjoern/statsd_tags.c
+++ bjoern/statsd_tags.c
@@ -3,24 +3,12 @@
 
 #define MAX_MSG_LEN_WITH_TAGS 1000
 
-/* will change the original string */
-static void cleanup(char *stat)
-{
-  char *p;
-  for (p = stat; *p; p++) {
-    if (*p == ':' || *p == '|' || *p == '@') {
-      *p = '_';
-    }
-  }
-}
-
 static int send_stats_with_tags(statsd_link* link, char *stat, size_t value, const char* type, const char* tags)
 {
   if(!link) return 0;
 
   char message[MAX_MSG_LEN_WITH_TAGS];
 
-  cleanup(stat);
   snprintf(message, MAX_MSG_LEN_WITH_TAGS, "%s%s:%zd|%s|#%s", link->ns ? link->ns : "", stat, value, type, tags ? tags : "");
 
   return statsd_send(link, message);
