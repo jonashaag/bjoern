73e552fc3e2b3fc09700aa69c1b5700f1d461ca6
Goldstein <mouse-art@ya.ru>
2019-12-04 16:54:40 +0300

    Fix segmentation fault in old interpreters (#173)
    * Fix segmentation fault in old interpreters
    Due to bug with constructing empty strings via PyString_FromFormat(),
    bjoern crashed Python interpreter. It happens on Python 2 interpreters
    before 2.7.16 and Python 3 interpreters before 3.5.
    https://bugs.python.org/issue33817
    
    * Add comments about empty Python string creation

--- bjoern/request.c
+++ bjoern/request.c
@@ -398,14 +398,14 @@ void _initialize_request_module(ServerInfo* server_info)
       PyDict_SetItemString(wsgi_base_dict, "SERVER_NAME", server_info->host);
 
       if (server_info->port == Py_None) {
-      PyDict_SetItemString(wsgi_base_dict, "SERVER_PORT", _PEP3333_String_FromFormat(""));
+      PyDict_SetItemString(wsgi_base_dict, "SERVER_PORT", _PEP3333_String_Empty());
       } else {
         PyDict_SetItemString(wsgi_base_dict, "SERVER_PORT", _PEP3333_String_FromFormat("%i", server_info->port));
       }
      } else {
       /* SERVER_NAME is required, but not usefull with UNIX type sockets */
-      PyDict_SetItemString(wsgi_base_dict, "SERVER_NAME", _PEP3333_String_FromFormat(""));
-      PyDict_SetItemString(wsgi_base_dict, "SERVER_PORT", _PEP3333_String_FromFormat(""));
+      PyDict_SetItemString(wsgi_base_dict, "SERVER_NAME", _PEP3333_String_Empty());
+      PyDict_SetItemString(wsgi_base_dict, "SERVER_PORT", _PEP3333_String_Empty());
     }
   }
 }
